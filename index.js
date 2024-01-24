310ae71f2dbb5f77bfec6a5029d6058777969aba
majidkhanhangu <a.majid5560@gmail.com>
2024-01-24 11:56:05 +0500

    animation

---
+++ index.js
@@ -0,0 +1,14 @@
+// to add typing animation with help of javascript//
+
+var messageArray = ["Frontened Developer .","YouTuber",];
+var textPosition = 1;
+var speed = 100;
+
+typewriter = () => {
+  document.querySelector("#multipleStrings").
+  innerHTML = messageArray[0].substring(0,textPosition) + "<span>\u25ae</span>";
+
+  if (textPosition++ != messageArray[0].length)
+  setTimeout(typewriter, speed);
+}
+window.addEventListener("load", typewriter);
